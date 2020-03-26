12c2e8e7d7e1f9cafd8159701c261d2594741871
IkpemosiArnold <Ikpemosi@protonmail.com>
2020-03-26 18:00:16 +0100

    First Start Dot ng JS task

---
+++ script.js
@@ -0,0 +1,7 @@
+var name="Ikpemosi Arnold";
+var courses=["Design","Frontend"];
+var number;
+for(number = 2; number<=200; number+=2){
+    console.log(number);
+}
+console.log(name + " " + courses.toString());
